9810e65603467c632e2755e21f41b4390cb02635
syed zubair hashmi <zubairhashmi423@gmail.com>
2024-10-26 08:21:19 -0700

    usereducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
 
+function reducer(state, action){
+ 
+  // switch (state.type) {
+  //   case :
+      
+  //     break;
+   
+  //   default:
+  //     break;
+  //  }
+}
+
+
 function App() {
+  
+   const intialValue = {
+   counter:0,
+   isDark:false,
+   likes:0,
+   isLogedin:false
+   }
+
+ const[state, dispatch] = useReducer(reducer,intialValue)
+
+
+
+
+ const incremethandler=()=>{
+  dispatch(1)
+}
+ const decremethandler=()=>{
+  dispatch(-1)
+ }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Heading{state}</h1>
+     <button>theme</button>    
+     <div>
+      <button onClick={incremethandler}>Increment</button>
+      <button onClick={decremethandler}>decrement</button>
+      <button onClick={decremethandler}>login</button>
+      <button onClick={decremethandler}>logout</button>
+     </div>
     </div>
   );
 }
