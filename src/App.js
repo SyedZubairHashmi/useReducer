8383f06afb1cce85d7c5e3148f5ad7b30ae0936e
syed zubair hashmi <zubairhashmi423@gmail.com>
2024-11-10 19:17:19 -0800

    reducer

--- src/App.js
+++ src/App.js
@@ -1,52 +1,73 @@
 import { useReducer } from 'react';
 import './App.css';
 
-function reducer(state, action){
- 
-  // switch (state.type) {
-  //   case :
-      
-  //     break;
-   
-  //   default:
-  //     break;
-  //  }
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { ...state, counter: state.counter + 1 };
+    case "decrement":
+      return { ...state, counter: state.counter - 1 };
+    case "toggleTheme":
+      return { ...state, isDark: !state.isDark };
+    case "login":
+      return { ...state, isLogedin: true };
+    case "logout":
+      return { ...state, isLogedin: false };
+    default:
+      return state;
+  }
 }
-
 
 function App() {
+  const initialValue = {
+    counter: 0,
+    isDark: false,
+    likes: 0,
+    isLogedin: false
+  };
+  const [state, dispatch] = useReducer(reducer, initialValue);
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
   
-   const intialValue = {
-   counter:0,
-   isDark:false,
-   likes:0,
-   isLogedin:false
-   }
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
- const[state, dispatch] = useReducer(reducer,intialValue)
+  const toggleThemeHandler = () => {
+    dispatch({ type: "toggleTheme" });
+  };
 
+  const loginHandler = () => {
+    dispatch({ type: "login" });
+  };
 
-
-
- const incremethandler=()=>{
-  dispatch(1)
-}
- const decremethandler=()=>{
-  dispatch(-1)
- }
-
-
+  const logoutHandler = () => {
+    dispatch({ type: "logout" });
+  };
 
   return (
     <div className="App">
-     <h1>Heading{state}</h1>
-     <button>theme</button>    
-     <div>
-      <button onClick={incremethandler}>Increment</button>
-      <button onClick={decremethandler}>decrement</button>
-      <button onClick={decremethandler}>login</button>
-      <button onClick={decremethandler}>logout</button>
-     </div>
+      <h1>Counter: {state.counter}</h1>
+      <h2>Theme: {state.isDark ? "Dark" : "Light"}</h2>
+      <h2 
+        style={{
+          color: state.isLogedin ? "green" : "red",
+          border: state.isLogedin ? "2px solid green" : "2px solid red",
+          padding: "5px",
+          backgroundColor: state.isLogedin ? "black" : "yellow",
+        }}
+      >
+        Status: {state.isLogedin ? "Logged In" : "Logged Out"}
+      </h2>
+      <button onClick={toggleThemeHandler}>Toggle Theme</button>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={loginHandler}>Login</button>
+        <button onClick={logoutHandler}>Logout</button>
+      </div>
     </div>
   );
 }
